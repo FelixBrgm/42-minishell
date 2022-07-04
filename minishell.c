e9f5f1c4c7eb662a0e4eb7e70318419b416821c8
Doush Hamdiev <dhamdiev@1-B-32.42heilbronn.de>
2022-07-04 20:04:14 +0200

    enabled while loop in minishell.c

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: dhamdiev <dhamdiev@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 14:14:50 by fbruggem          #+#    #+#             */
-/*   Updated: 2022/07/04 20:01:34 by dhamdiev         ###   ########.fr       */
+/*   Updated: 2022/07/04 20:03:49 by dhamdiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	main(int argc, char **argv, char **env)
 
 	set_to_default(&global, env);
 	builtin_export(NULL, global.env);
-	// while (1)
-	// 	input_read(&global);
+	while (1)
+		input_read(&global);
 	// set_to_default(global);
 	// while (1)
 	// {
