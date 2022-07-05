0cd8797ab355a99465423c032ec6d12d419f217a
Doush Hamdiev <dhamdiev@1-B-32.42heilbronn.de>
2022-07-05 20:43:56 +0200

    fixed the mistake

--- src/children/children_exec.c
+++ src/children/children_exec.c
@@ -6,7 +6,7 @@
 /*   By: dhamdiev <dhamdiev@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:26:52 by fbruggem          #+#    #+#             */
-/*   Updated: 2022/07/05 20:40:38 by dhamdiev         ###   ########.fr       */
+/*   Updated: 2022/07/05 20:43:23 by dhamdiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	children_exec(t_global *global)
 	int	fd_current[2];
 	int	fd_temp[2];
 	int	i;
-	int	ex_status;
 
 	i = 0;
 	fd_current[0] = -1;
@@ -40,11 +39,11 @@ void	children_exec(t_global *global)
 		{
 			global->children[i]->fd_in = fd_current[0];
 			global->children[i]->fd_out = -1;
-			child_exec(global->children[i], global->env, &ex_status);
+			child_exec(global->children[i], global->env, &global->ex_status);
 			close(fd_current[0]);
 		}
 		i++;
 	}
-	if (WIFEXITED(ex_status))
-		printf("EXIT CODE WAS %d\n", WEXITSTATUS(&global->ex_status));
+	if (WIFEXITED(global->ex_status))
+		printf("EXIT CODE WAS %d\n", WEXITSTATUS(global->ex_status));
 }
